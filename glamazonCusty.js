8ae4930bcae37aae1ac241f56225c4f97674d62e
Nackles <45973617+Nackles@users.noreply.github.com>
2019-04-02 16:20:15 -0700

    oh shit more's working

--- glamazonCusty.js
+++ glamazonCusty.js
@@ -39,7 +39,7 @@ function sellGoods() {
     inquirer.prompt([
         {
             type: "input",
-            message: "What is the Product ID of the item you wish to purchase with your Real Mars Dollars?",
+            message: "What is the Item ID of the item you wish to purchase with your Real Mars Dollars?",
             name: "buyid"
         },
         {
@@ -48,15 +48,28 @@ function sellGoods() {
             name: "buyamt"
         }
     ]).then(function (user, res) {
+        console.log("is this giving gold biscuits", serverTracker[user.buyid - 1].stock_quantity)
         if (!user.buyid) {
             console.log("We don't carry them goods. Try to buy something I've got.")
             sellGoods();
-        } else if (user.buyamt < serverTracker[user.buyid-1].stock_quantity) {
-            // TODO: SUBTRACT THE AMOUNT PURCHSED FROM STOCK_QUANTITY
+        } else if (user.buyamt < serverTracker[user.buyid].stock_quantity) {
+            // TODO: YA FUCKIN DID IT KIDDO
             console.log("That's an item that exists.")
-            
+            var one = serverTracker[user.buyid - 1].stock_quantity
+            var two = user.buyamt
+            var newtotal = one - two;
+            connection.query("UPDATE products SET ? WHERE ?",
+                [{
+                    stock_quantity: newtotal 
+                },
+                {
+                    item_id: user.buyid
+                }],
+                function (err, result) {
+                    if (err) throw err
+                })
         } else {
-            console.log("We don't have that many. Were the pit of "+serverTracker[user.buyid-1]+" so bottomless!");
+            console.log("We don't have that many. Were the pit of " + serverTracker[user.buyid - 1] + " so bottomless!");
         }
     })
 
